d546a72d58d289a34120692343b85bf004120d2e
veselovoleg <veselovoleg.msk@yandex.ru>
2019-04-08 10:35:09 +0300

    alghoritms

--- scripts_ts/alghoritms.ts
+++ scripts_ts/alghoritms.ts
@@ -19,7 +19,30 @@ for(let i = 1; i <= 100; i++){
         results.get('devidedBy3and5').push(i);
         results.get('devidedBy3').push(i);
         results.get('devidedBy5').push(i);
-    } else devidedBy3 ? results.get('devidedBy3').push(i) : devidedBy5 ? results.get('devidedBy5').push(i) : results.get('others').push(i);
+    } else devidedBy3 
+    ? results.get('devidedBy3').push(i) : devidedBy5 
+    ? results.get('devidedBy5').push(i) : results.get('others').push(i);
 }
 
 console.log(results);
+
+//Array sotrings
+let names_arr: ReadonlyArray<string> = ['Tom', 'Jerry', 'Abaddon', 'ZombieBoy'];
+let names_arr_copy = names_arr.slice().sort(); //Radonly - можем сортировать копию: сделанную при помощи слайса
+console.log({ names_arr_copy }); // => { names_arr_copy: ["Abaddon", "Jerry", "Tom", "ZombieBoy"]}
+
+//numbers
+function sort_by_nums (num1: number, num2: number):number{
+    return num1 - num2
+};
+
+let any_nums = [1, 99, 67, 2, 343, 0];
+console.log(any_nums.sort(sort_by_nums)); //=> [0, 1, 2, 67, 99, 343]
+
+let students = [
+    { name: 'Logan', birth_year: 1889},
+    { name: 'Peter', birth_year: 1991},
+    { name: 'Otto', birth_year: 1965},
+];
+
+console.log(students.sort((a, b) => a.birth_year - b.birth_year));
